8d105f9f050c2630afd6bfde3df70d6ef13437aa
biranchi007 <biranchi007@gmail.com>
2020-04-10 12:17:18 +0530

    1 file added

---
+++ Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+//import Footer from './Footer'
+
+function Footer() {
+  return (
+    <div>
+      <footer>Footer</footer>
+    </div>
+  );
+}
+
+export default Footer;
