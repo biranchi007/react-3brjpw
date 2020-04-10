dda1e31afd361ed20048a3cd61eab400d99d01ed
biranchi007 <biranchi007@gmail.com>
2020-04-10 12:15:48 +0530

    1 file modified

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import MyInfo from './components/lesson1/MyInfoFile1'
 
-import App from './App'
+import App from './App' 
 
 import Footer from './Footer'
 
