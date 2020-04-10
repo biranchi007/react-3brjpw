53754c6c02e84fd98170d47bf34a425872c2a155
biranchi007 <biranchi007@gmail.com>
2020-04-10 12:28:14 +0530

    1 file modified, 3 added, 2 deleted

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from "react";
-import Footer from "./Footer";
-import Header from "./Header";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import MainComponent from "./components/MainComponent";
 
 function App() {
   return (
     <div>
       <h1>EntryPoint</h1>
       <Header />
+      <MainComponent />
       <Footer />
     </div>
   );
---
+++ components/MainComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+//import Footer from './Footer'
+
+function Header() {
+  return (
+    <div>
+      <main>Main Component</main>
+    </div>
+  );
+}
+
+export default Header;
